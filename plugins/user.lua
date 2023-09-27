19ba02ef29622a59506f02ce1c28df6adcd2c3db
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2023-09-27 11:59:35 -0500

    added vimtex

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,10 @@
 return {
+  {
+     "lervag/vimtex",
+    ft = { "tex", "bib" },
+    config = function() vim.g.vimtex_view_method = "zathura" end,
+  }
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
